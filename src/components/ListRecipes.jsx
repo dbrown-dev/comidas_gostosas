d3afedb3ad294e472df33a6358850686dbd65eee
David Brown <david.brown@windowslive.com>
2019-07-03 13:19:22 +1200

    changed listrecipes to class component

--- src/components/ListRecipes.jsx
+++ src/components/ListRecipes.jsx
@@ -5,10 +5,9 @@ import RecipeCard from './RecipeCard'
 import { getRecipesSummary } from '../utilities/api'
 import { setRecipesList } from '../actions/recipesList'
 
-
-
-const ListRecipes = ({ recipesList, classes, dispatch }) => {
-  const getRecipesList = async () => {
+class ListRecipes extends React.Component {
+  getRecipesList = async () => {
+    const { dispatch } = this.props
     try {
       const recipesListData = await getRecipesSummary()
       dispatch(setRecipesList(recipesListData))
@@ -16,22 +15,29 @@ const ListRecipes = ({ recipesList, classes, dispatch }) => {
       throw new Error('Error with fetching  recipes list from API')
     }
   }
-  getRecipesList()
 
-  return (
-    <Container maxWidth="md" className={classes.recipeList}>
-      <Grid container spacing={6}>
-        {recipesList.map(recipe => (
-          <Grid item key={recipe.id} xs={12} sm={6} md={4}>
-            <RecipeCard classes={classes} recipe={recipe} />
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
-  )
+  componentDidMount() {
+    this.getRecipesList()
+  }
+
+  render() {
+    const { recipesList, classes } = this.props
+    console.log(recipesList)
+    return (
+      <Container maxWidth="md" className={classes.recipeList}>
+        <Grid container spacing={6}>
+          {recipesList[0] && recipesList[0].map(recipe => (
+            <Grid item key={recipe.id} xs={12} sm={6} md={4}>
+              <RecipeCard classes={classes} recipe={recipe} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    )
+  }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     recipesList: state.recipesList
   }
